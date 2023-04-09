6c50a6678a945fdb41701a7b7d5d77ad4ed09270
RHITHIK <123656899+rhithikonline@users.noreply.github.com>
2023-04-09 08:44:34 +0530

    Add files via upload
    To cheak a number is even or odd .

---
+++ odd or even.py
@@ -0,0 +1,9 @@
+#only for biginner
+#this program to find a number odd or even
+
+number=int(input("enter a number  to find odd or even"))
+
+if(number%2)==0:
+    print("The number is even")
+else:
+    print("The number is odd")    
